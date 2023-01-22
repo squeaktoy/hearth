d9be443f9729782642c2773f9b39f4be03234b5e
marceline-cramer <cramermarceline@gmail.com>
2023-01-21 21:36:51 -0700

    Add initial hearth-rpc crate

---
+++ crates/hearth-rpc/src/lib.rs
@@ -0,0 +1,21 @@
+use remoc::rtc::{remote, CallError};
+
+pub type CallResult<T> = Result<T, CallError>;
+
+/// An interface for acquiring access to a client's remote APIs.
+///
+/// This is an example of the [Service Locator design pattern](https://gameprogrammingpatterns.com/service-locator.html).
+/// This is considered an anti-pattern by some because services acquired
+/// through it cannot be easily tested. However, this is not an issue in this
+/// usecase because all this interface provides access to are procedural client
+/// implementations to the real remote implementation, which could be made
+/// testable with mocks at no consequence on this interface.
+#[remote]
+pub trait ClientApiProvider {
+    async fn get_process_api(&self) -> CallResult<ProcessApiClient>;
+}
+
+#[remote]
+pub trait ProcessApi {
+    async fn print_hello_world(&self) -> CallResult<()>;
+}
