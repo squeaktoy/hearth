dddd22604666c1c3d79aec299988efd5582d20a0
marceline-cramer <cramermarceline@gmail.com>
2023-01-21 22:23:45 -0700

    Plumb RPC print_hello_world through cognito in a gnarly way

--- crates/hearth-cognito/src/lib.rs
+++ crates/hearth-cognito/src/lib.rs
@@ -1,36 +1,64 @@
+use hearth_rpc::ProcessApi;
 use hearth_wasm::WasmLinker;
-use wasmtime::Linker;
+use wasmtime::{Caller, Linker};
 
 /// This contains all script-accessible process-related stuff.
-pub struct Cognito {}
+pub struct Cognito<Api> {
+    pub api: Api,
+}
 
-impl Cognito {
-    pub fn print_hello_world(&self) {
-        println!("Hello, world!");
+impl<Api: ProcessApi + Send + Sync> Cognito<Api> {
+    pub async fn print_hello_world(&self) {
+        self.api.print_hello_world().await.unwrap();
     }
 }
 
-impl<T: AsRef<Cognito> + 'static> WasmLinker<T> for Cognito {
+impl<Api, T> WasmLinker<T> for Cognito<Api>
+where
+    Api: ProcessApi + Send + Sync + 'static,
+    T: AsRef<Cognito<Api>> + Send + 'static,
+{
     const MODULE_NAME: &'static str = "cognito";
 
     fn add_to_linker(linker: &mut Linker<T>) {
-        Self::wrap_func(
-            linker,
-            "print_hello_world",
-            move |caller: wasmtime::Caller<'_, T>| {
-                caller.data().as_ref().print_hello_world();
-            },
-        );
+        linker
+            .func_wrap0_async("cognito", "print_hello_world", |caller: Caller<'_, T>| {
+                Box::new(print_hello_world(caller))
+            })
+            .unwrap();
     }
+}
+
+pub async fn print_hello_world<Api, T>(caller: wasmtime::Caller<'_, T>)
+where
+    Api: ProcessApi + Send + Sync,
+    T: AsRef<Cognito<Api>> + Send,
+{
+    let cognito = caller.data().as_ref();
+    cognito.print_hello_world().await;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use hearth_rpc::{remoc, CallResult};
+    use remoc::rtc::async_trait;
+
+    struct MockProcessApi;
+
+    #[async_trait]
+    impl ProcessApi for MockProcessApi {
+        async fn print_hello_world(&self) -> CallResult<()> {
+            println!("Hello, world!");
+            Ok(())
+        }
+    }
+
     #[test]
     fn host_works() {
-        let cognito = Cognito {};
+        let api = MockProcessApi;
+        let cognito = Cognito { api };
         cognito.print_hello_world();
     }
 }
