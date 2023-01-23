56af49ada8536ba52c971c335b6cb0c7b1526098
marceline-cramer <cramermarceline@gmail.com>
2023-01-22 18:35:09 -0700

    Cleanup font-mud lib.rs

--- crates/font-mud/src/lib.rs
+++ crates/font-mud/src/lib.rs
@@ -1,26 +1,24 @@
 mod error;
-mod glyph_bitmap;
 mod glyph_atlas;
+mod glyph_bitmap;
 
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
     use crate::glyph_atlas::GlyphAtlas;
+    use std::fs::File;
 
-    const RUN_TEST: bool = false;
     #[test]
     fn test() {
-        if RUN_TEST {
-            let face = ttf_parser::Face::parse(notosans::REGULAR_TTF, 0).unwrap();
-            let (glyph_atlas, glyph_shape_errors) = GlyphAtlas::new(&face).unwrap();
-            save_bitmap_and_preview("lets", "go", "gamers", &glyph_atlas.bitmap.data);
-        }
+        let face = ttf_parser::Face::parse(notosans::REGULAR_TTF, 0).unwrap();
+        let (glyph_atlas, _glyph_shape_errors) = GlyphAtlas::new(&face).unwrap();
+        save_bitmap_and_preview("lets", "go", "gamers", &glyph_atlas.bitmap.data);
     }
+
     fn save_bitmap_and_preview<T>(pfx: &str, name: &str, sfx: &str, bitmap: &msdfgen::Bitmap<T>)
-        where
-            T: msdfgen::PngColorType + Copy,
-            T::PngPixelType: From<T>,
-            msdfgen::Gray<f32>: msdfgen::RenderTarget<T>,
+    where
+        T: msdfgen::PngColorType + Copy,
+        T::PngPixelType: From<T>,
+        msdfgen::Gray<f32>: msdfgen::RenderTarget<T>,
     {
         let mut bitmap = bitmap.clone();
         bitmap.flip_y();
