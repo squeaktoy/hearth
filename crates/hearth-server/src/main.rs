9635d8b84614ef8a22e0ffe71d3220069f668499
marceline-cramer <cramermarceline@gmail.com>
2023-01-29 12:47:36 -0700

    server: impl dummy ClientApiProvider and pass over encrypted remoc to client

--- crates/hearth-server/src/main.rs
+++ crates/hearth-server/src/main.rs
@@ -3,6 +3,11 @@ use std::sync::Arc;
 
 use clap::Parser;
 use hearth_network::auth::ServerAuthenticator;
+use hearth_rpc::{
+    CallResult, ClientApiProvider, ClientApiProviderClient, ClientApiProviderServerShared,
+    ProcessApiClient,
+};
+use remoc::rtc::{async_trait, ServerShared};
 use tokio::net::{TcpListener, TcpStream};
 use tracing::{error, info};
 
@@ -70,4 +75,43 @@ async fn on_accept(
     };
 
     info!("Successfully authenticated");
+    use hearth_network::encryption::{AsyncDecryptor, AsyncEncryptor, Key};
+    let client_key = Key::from_client_session(&session_key);
+    let server_key = Key::from_server_session(&session_key);
+
+    let (client_rx, client_tx) = tokio::io::split(client);
+    let client_rx = AsyncDecryptor::new(&client_key, client_rx);
+    let client_tx = AsyncEncryptor::new(&server_key, client_tx);
+
+    use remoc::rch::base::{Receiver, Sender};
+    let cfg = remoc::Cfg::default();
+    let (conn, mut tx, _rx): (_, Sender<ClientApiProviderClient>, Receiver<()>) =
+        match remoc::Connect::io(cfg, client_rx, client_tx).await {
+            Ok(v) => v,
+            Err(err) => {
+                error!("Remoc connection failure: {:?}", err);
+                return;
+            }
+        };
+
+    tokio::spawn(conn);
+
+    let server = ClientApiProviderImpl;
+    let server = std::sync::Arc::new(server);
+    let (provider, provider_client) =
+        ClientApiProviderServerShared::<_, remoc::codec::Default>::new(server, 1024);
+    tokio::spawn(async move {
+        provider.serve(true).await;
+    });
+    tx.send(provider_client).await.unwrap();
+}
+
+struct ClientApiProviderImpl;
+
+#[async_trait]
+impl ClientApiProvider for ClientApiProviderImpl {
+    async fn get_process_api(&self) -> CallResult<ProcessApiClient> {
+        error!("ClientApiProviderImpl::get_process_api() is unimplemented");
+        Err(remoc::rtc::CallError::Dropped)
+    }
 }
