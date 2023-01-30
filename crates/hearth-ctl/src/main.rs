c1e7a58cff74649ac1aaca2950922681d34f4c6c
marceline-cramer <cramermarceline@gmail.com>
2023-01-30 12:42:17 -0700

    ctl: add tokio, clap, and workspace deps + Args parsing

--- crates/hearth-ctl/src/main.rs
+++ crates/hearth-ctl/src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Parser, Subcommand};
+
+/// Command-line interface (CLI) for interacting with a Hearth daemon over IPC.
+#[derive(Debug, Parser)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {}
+
 fn main() {
+    let args = Args::parse();
+
     println!("Hello, world!");
 }
