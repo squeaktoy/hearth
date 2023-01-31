74f10d1ea02de12fe4937f6dfd6b72f764d458c0
marceline-cramer <cramermarceline@gmail.com>
2023-01-30 19:10:33 -0700

    server: use core's init_logging()

--- crates/hearth-server/src/main.rs
+++ crates/hearth-server/src/main.rs
@@ -26,12 +26,7 @@ pub struct Args {
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-
-    let format = tracing_subscriber::fmt::format().compact();
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .event_format(format)
-        .init();
+    hearth_core::init_logging();
 
     let authenticator = ServerAuthenticator::from_password(args.password.as_bytes()).unwrap();
     let authenticator = Arc::new(authenticator);
