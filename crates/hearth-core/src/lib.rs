63e6ccb8bcd296d776a2ca9fb4d2d9c8499ed7e0
marceline-cramer <cramermarceline@gmail.com>
2023-01-30 19:07:07 -0700

    core: add init_logging() and PeerApiImpl

--- crates/hearth-core/src/lib.rs
+++ crates/hearth-core/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use hearth_rpc::*;
+use remoc::rtc::async_trait;
+use tracing::error;
+
+/// Helper function to set up console logging with reasonable defaults.
+pub fn init_logging() {
+    let format = tracing_subscriber::fmt::format().compact();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .event_format(format)
+        .init();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// The canonical [PeerApi] implementation, with full functionality.
+pub struct PeerApiImpl {
+    pub info: PeerInfo,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[async_trait]
+impl PeerApi for PeerApiImpl {
+    async fn get_info(&self) -> CallResult<PeerInfo> {
+        Ok(self.info.clone())
+    }
+
+    async fn get_process_store(&self) -> CallResult<ProcessStoreClient> {
+        error!("Process stores are unimplemented");
+        Err(remoc::rtc::CallError::RemoteForward)
     }
 }
