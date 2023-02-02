b4f742c5935aa707f6c0898250a52002e89b430d
marceline-cramer <cramermarceline@gmail.com>
2023-02-01 22:15:58 -0700

    core: spawn duplex connection for lump store tests

--- crates/hearth-core/src/api/lump_store.rs
+++ crates/hearth-core/src/api/lump_store.rs
@@ -37,9 +37,51 @@ mod tests {
         )
     }
 
-    #[test]
-    fn create_store() {
-        let _store = LumpStoreImpl::new();
+    /// LazyBlobs don't work locally, so this needs to be pulled off in order
+    /// to get them to work in these tests.
+    async fn spawn_store() -> LumpStoreClient {
+        use remoc::rch::base::{Receiver, Sender};
+
+        let (a, b) = tokio::io::duplex(4096);
+        let (a_rx, a_tx) = tokio::io::split(a);
+        let (b_rx, b_tx) = tokio::io::split(b);
+
+        let join_a = tokio::spawn(async move {
+            let (conn, tx, _rx): (_, Sender<LumpStoreClient>, Receiver<()>) =
+                remoc::Connect::io(Default::default(), a_rx, a_tx)
+                    .await
+                    .unwrap();
+            tokio::task::spawn(conn);
+            tx
+        });
+
+        let join_b = tokio::spawn(async move {
+            let (conn, _tx, rx): (_, Sender<()>, Receiver<LumpStoreClient>) =
+                remoc::Connect::io(Default::default(), b_rx, b_tx)
+                    .await
+                    .unwrap();
+            tokio::task::spawn(conn);
+            rx
+        });
+
+        let store = LumpStoreImpl::new();
+        let store = Arc::new(store);
+        let (store_server, store) = LumpStoreServerShared::new(store, 1024);
+
+        tokio::spawn(async move {
+            store_server.serve(true).await;
+        });
+
+        let mut tx = join_a.await.unwrap();
+        let mut rx = join_b.await.unwrap();
+
+        tx.send(store).await.unwrap();
+        rx.recv().await.unwrap().unwrap()
+    }
+
+    #[tokio::test]
+    async fn create_store() {
+        let _store = spawn_store();
     }
 
     #[tokio::test]
@@ -47,7 +89,7 @@ mod tests {
         const DATA: &[u8] = b"Hello, world!";
         let id = make_id(DATA);
         let data_blob = LazyBlob::new(DATA.into());
-        let store = LumpStoreImpl::new();
+        let store = spawn_store().await;
 
         let uploaded = store
             .upload_lump(Some(id), data_blob)
@@ -72,7 +114,7 @@ mod tests {
         const DATA: &[u8] = b"Hello, world!";
         let wrong = make_id(b"Wrong data!");
         let data_blob = LazyBlob::new(DATA.into());
-        let store = LumpStoreImpl::new();
+        let store = spawn_store().await;
         let result = store.upload_lump(Some(wrong), data_blob).await;
         match result {
             Err(ResourceError::BadParams) => {}
