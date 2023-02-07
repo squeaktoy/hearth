ba652afea36724843a039e6f8c69e40f7a5b359d
marceline-cramer <cramermarceline@gmail.com>
2023-02-06 20:28:34 -0700

    core: better logging defaults

--- crates/hearth-core/src/lib.rs
+++ crates/hearth-core/src/lib.rs
@@ -1,14 +1,22 @@
-use tracing::{debug, error, info};
+use tracing::{debug, error, info, Level};
+use tracing_subscriber::prelude::*;
 
 /// Implementations of the `hearth-rpc` crate's RPC interfaces.
 pub mod api;
 
 /// Helper function to set up console logging with reasonable defaults.
 pub fn init_logging() {
-    let format = tracing_subscriber::fmt::format().compact();
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .event_format(format)
+    let filter = tracing_subscriber::filter::Targets::new()
+        .with_target("wgpu", Level::INFO)
+        .with_target("wgpu_core", Level::WARN)
+        .with_target("wgpu_hal", Level::WARN)
+        .with_default(Level::DEBUG);
+
+    let format = tracing_subscriber::fmt::layer().compact();
+
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(format)
         .init();
 }
 
