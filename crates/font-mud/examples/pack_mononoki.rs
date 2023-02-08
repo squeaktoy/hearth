3337f6b9ac2f37a8500610e4f2d0bf658b73a4b3
marceline-cramer <cramermarceline@gmail.com>
2023-02-07 22:03:05 -0700

    font-mud: swap out noto sans for mononoki

--- crates/font-mud/examples/pack_mononoki.rs
+++ crates/font-mud/examples/pack_mononoki.rs
@@ -2,10 +2,11 @@ use font_mud::glyph_atlas::GlyphAtlas;
 use std::fs::File;
 
 fn main() {
-    let face = ttf_parser::Face::parse(notosans::REGULAR_TTF, 0).unwrap();
+    let ttf_src = include_bytes!("../../../resources/mononoki/mononoki-Regular.ttf");
+    let face = ttf_parser::Face::parse(ttf_src, 0).unwrap();
     let (glyph_atlas, _glyph_shape_errors) = GlyphAtlas::new(&face).unwrap();
     let bitmap = &glyph_atlas.bitmap;
-    let output = File::create("noto-sans.png").unwrap();
+    let output = File::create("mononoki.png").unwrap();
     let mut encoder = png::Encoder::new(output, bitmap.width as u32, bitmap.height as u32);
     encoder.set_color(png::ColorType::Rgba);
     encoder.set_depth(png::BitDepth::Eight);
