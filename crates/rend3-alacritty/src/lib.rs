fc7b5e5f548feb723982fd7d1e2aa2b9df14f640
marceline-cramer <cramermarceline@gmail.com>
2023-02-07 20:53:12 -0700

    alacritty: add Alacrittyroutine::add_to_graph

--- crates/rend3-alacritty/src/lib.rs
+++ crates/rend3-alacritty/src/lib.rs
@@ -1,5 +1,8 @@
+use rend3::graph::{
+    RenderGraph, RenderPassDepthTarget, RenderPassTarget, RenderPassTargets, RenderTargetHandle,
+};
 use rend3::Renderer;
-use wgpu::{RenderPipeline, TextureFormat};
+use wgpu::{Color, RenderPipeline, TextureFormat};
 
 pub struct AlacrittyRoutine {
     pipeline: RenderPipeline,
@@ -60,5 +63,39 @@ impl AlacrittyRoutine {
             });
 
         Self { pipeline }
+    }
+
+    pub fn add_to_graph<'node>(
+        &'node mut self,
+        graph: &mut RenderGraph<'node>,
+        output: RenderTargetHandle,
+        depth: RenderTargetHandle,
+    ) {
+        let mut builder = graph.add_node("alacritty");
+        let output_handle = builder.add_render_target_output(output);
+        let depth_handle = builder.add_render_target_input(depth);
+        let rpass_handle = builder.add_renderpass(RenderPassTargets {
+            targets: vec![RenderPassTarget {
+                color: output_handle,
+                clear: Color::BLACK,
+                resolve: None,
+            }],
+            depth_stencil: Some(RenderPassDepthTarget {
+                target: rend3::graph::DepthHandle::RenderTarget(depth_handle),
+                depth_clear: None,
+                stencil_clear: None,
+            }),
+        });
+
+        let pt_handle = builder.passthrough_ref(self);
+
+        builder.build(
+            move |pt, renderer, encoder_or_pass, temps, ready, graph_data| {
+                let this = pt.get(pt_handle);
+                let rpass = encoder_or_pass.get_rpass(rpass_handle);
+                rpass.set_pipeline(&this.pipeline);
+                rpass.draw(0..3, 0..1);
+            },
+        );
     }
 }
