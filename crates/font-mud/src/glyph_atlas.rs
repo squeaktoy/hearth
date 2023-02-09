6b0882e1b8bae404afcf766204e71885a64446d0
marceline-cramer <cramermarceline@gmail.com>
2023-02-08 19:26:59 -0700

    font-mud: correctly scale and anchor glyphs

--- crates/font-mud/src/glyph_atlas.rs
+++ crates/font-mud/src/glyph_atlas.rs
@@ -1,6 +1,6 @@
 use crate::error::{FontError, FontResult, GlyphShapeError};
 use crate::glyph_bitmap::{GlyphBitmap, GlyphMtsdf};
-use glam::Vec2;
+use glam::{UVec2, Vec2};
 use msdfgen::Range;
 use rect_packer::Packer;
 use ttf_parser::{Face, GlyphId};
@@ -12,9 +12,9 @@ pub struct GlyphVertex {
 }
 
 pub struct GlyphInfo {
-    pub position: (usize, usize),
-    pub size: (usize, usize),
-    pub anchor: (f32, f32),
+    pub position: UVec2,
+    pub size: UVec2,
+    pub anchor: Vec2,
     pub vertices: [GlyphVertex; 4],
 }
 
@@ -33,10 +33,15 @@ impl GlyphAtlas {
     pub fn new(face: &Face) -> FontResult<(GlyphAtlas, Vec<GlyphShapeError>)> {
         let mut glyphs = vec![];
         let mut glyph_shape_errors = vec![];
-        let scale = Self::PX_PER_EM / face.units_per_em() as f64;
         for c in 0..face.number_of_glyphs() {
-            let glyph =
-                GlyphMtsdf::generate(scale, Self::RANGE, Self::ANGLE_THRESHOLD, face, GlyphId(c));
+            let glyph = GlyphMtsdf::generate(
+                face.units_per_em() as f64,
+                Self::PX_PER_EM,
+                Self::RANGE,
+                Self::ANGLE_THRESHOLD,
+                face,
+                GlyphId(c),
+            );
 
             match glyph {
                 Ok(glyph) => {
@@ -54,8 +59,8 @@ impl GlyphAtlas {
             }
         }
         let mut packer = Self::generate_packer(&glyphs);
-        let width = packer.config().width as usize;
-        let height = packer.config().height as usize;
+        let width = packer.config().width as u32;
+        let height = packer.config().height as u32;
         let texture_size = Vec2::new(width as f32, height as f32);
         let mut bitmap = GlyphBitmap::new(width, height);
         let mut glyph_info = vec![];
@@ -67,41 +72,41 @@ impl GlyphAtlas {
                 Some(glyph) => {
                     if let Some(rect) = packer.pack(glyph.width as i32, glyph.height as i32, false)
                     {
-                        glyph.copy_to(&mut bitmap, rect.x as usize, rect.y as usize);
+                        glyph.copy_to(&mut bitmap, rect.x as u32, rect.y as u32);
 
-                        let anchor = Vec2::ZERO;
+                        let scale = (1.0 / glyph.px_per_em) as f32;
+                        let offset = glyph.anchor;
+                        println!("{} {:?}", scale, offset);
 
                         let position = Vec2::new(rect.x as f32, rect.y as f32);
                         let position = position / texture_size;
 
                         let size = Vec2::new(glyph.width as f32, glyph.height as f32);
-                        let size = size / texture_size;
-
                         let v1 = Vec2::ZERO;
                         let v2 = Vec2::new(size.x, 0.0);
                         let v3 = Vec2::new(0.0, size.y);
                         let v4 = size;
 
                         glyph_info.push(Some(GlyphInfo {
-                            position: (rect.x as usize, rect.y as usize),
-                            size: (glyph.width, glyph.height),
-                            anchor: (0.0, 0.0),
+                            position: UVec2::new(rect.x as u32, rect.y as u32),
+                            size: UVec2::new(glyph.width, glyph.height),
+                            anchor: offset,
                             vertices: [
                                 GlyphVertex {
-                                    position: v1 - anchor,
-                                    tex_coords: v1 + position,
+                                    position: v1 * scale - offset,
+                                    tex_coords: v1 / texture_size + position,
                                 },
                                 GlyphVertex {
-                                    position: v2 - anchor,
-                                    tex_coords: v2 + position,
+                                    position: v2 * scale - offset,
+                                    tex_coords: v2 / texture_size + position,
                                 },
                                 GlyphVertex {
-                                    position: v3 - anchor,
-                                    tex_coords: v3 + position,
+                                    position: v3 * scale - offset,
+                                    tex_coords: v3 / texture_size + position,
                                 },
                                 GlyphVertex {
-                                    position: v4 - anchor,
-                                    tex_coords: v4 + position,
+                                    position: v4 * scale - offset,
+                                    tex_coords: v4 / texture_size + position,
                                 },
                             ],
                         }))
--- crates/font-mud/src/glyph_bitmap.rs
+++ crates/font-mud/src/glyph_bitmap.rs
@@ -8,7 +8,7 @@ use ttf_parser::{Face, GlyphId};
 pub struct GlyphMtsdf {
     pub bitmap: GlyphBitmap,
     pub anchor: Vec2,
-    pub scale: f32,
+    pub px_per_em: f64,
 }
 
 impl Deref for GlyphMtsdf {
@@ -21,7 +21,8 @@ impl Deref for GlyphMtsdf {
 
 impl GlyphMtsdf {
     pub fn generate(
-        scale: f64,
+        units_per_em: f64,
+        px_per_em: f64,
         range: Range<f64>,
         angle_threshold: f64,
         face: &Face,
@@ -33,18 +34,18 @@ impl GlyphMtsdf {
             .ok_or(FontError::GlyphShape(GlyphShapeError(glyph)))?;
         shape.edge_coloring_simple(angle_threshold, 0);
         let bounds = shape.get_bound();
-        let width = (bounds.width() * scale).ceil() as usize + 2;
-        let height = (bounds.height() * scale).ceil() as usize + 2;
+        let px_per_unit = px_per_em / units_per_em;
+        let width = (bounds.width() * px_per_unit).ceil() as u32 + 2;
+        let height = (bounds.height() * px_per_unit).ceil() as u32 + 2;
         let framing = bounds
             .autoframe(width as u32, height as u32, range, None)
             .ok_or(FontError::AutoFraming {
                 glyph,
-                width,
-                height,
+                width: width as usize,
+                height: height as usize,
                 range,
             })?;
-        println!("{:#?}", framing);
-        let mut bitmap = Bitmap::<Rgba<f32>>::new(width as u32, height as u32);
+        let mut bitmap = Bitmap::<Rgba<f32>>::new(width, height);
         shape.generate_mtsdf(&mut bitmap, framing, config);
         shape.correct_sign(&mut bitmap, framing, FillRule::default());
         shape.correct_msdf_error(&mut bitmap, framing, config);
@@ -65,22 +66,23 @@ impl GlyphMtsdf {
                 width,
                 height,
             },
-            anchor: Vec2::ZERO,
-            scale: 0.0,
+            anchor: Vec2::new(framing.translate.x as f32, framing.translate.y as f32)
+                / units_per_em as f32,
+            px_per_em: px_per_em * px_per_unit / framing.scale.x,
         })
     }
 }
 
 pub struct GlyphBitmap {
     pub data: Vec<u32>,
-    pub width: usize,
-    pub height: usize,
+    pub width: u32,
+    pub height: u32,
 }
 
 impl GlyphBitmap {
-    pub fn new(width: usize, height: usize) -> Self {
+    pub fn new(width: u32, height: u32) -> Self {
         Self {
-            data: vec![0; width * height],
+            data: vec![0; (width * height) as usize],
             width,
             height,
         }
@@ -95,17 +97,19 @@ impl GlyphBitmap {
         }
     }
 
-    pub fn copy_to(&self, dst: &mut GlyphBitmap, x: usize, y: usize) {
+    pub fn copy_to(&self, dst: &mut GlyphBitmap, x: u32, y: u32) {
         if self.width + x > dst.width || self.height + y > dst.height {
             panic!("copy_to out-of-bounds");
         }
 
-        let mut cursor = y * dst.width + x;
-        for y in 0..self.height {
-            let src_range = (y * self.width)..((y + 1) * self.width);
-            let dst_range = cursor..(cursor + self.width);
+        let mut src_cursor = 0;
+        let mut dst_cursor = (y * dst.width + x) as usize;
+        for _ in 0..self.height {
+            let src_range = src_cursor..(src_cursor + self.width as usize);
+            let dst_range = dst_cursor..(dst_cursor + self.width as usize);
             dst.data[dst_range].copy_from_slice(&self.data[src_range]);
-            cursor += dst.width;
+            src_cursor += self.width as usize;
+            dst_cursor += dst.width as usize;
         }
     }
 }
