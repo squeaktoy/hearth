b6fa4a035c7b5f54488c7a702e24dc396e9d6b80
marceline-cramer <cramermarceline@gmail.com>
2023-02-13 21:01:53 -0700

    core: impl PeerApi for runtime and serve process and lump stores

--- crates/hearth-core/src/runtime.rs
+++ crates/hearth-core/src/runtime.rs
@@ -3,10 +3,15 @@ use std::collections::HashMap;
 use std::future::Future;
 use std::sync::Arc;
 
-use hearth_rpc::{hearth_types::*, PeerProviderClient};
+use hearth_rpc::remoc::rtc::ServerShared;
+use hearth_rpc::*;
+use hearth_types::PeerId;
+use remoc::rtc::async_trait;
 use tracing::{debug, error};
 
+use crate::api::lump_store::LumpStoreImpl;
 use crate::asset::{AssetLoader, AssetStore};
+use crate::process::ProcessStoreImpl;
 
 pub trait Plugin: 'static {
     fn build(&mut self, builder: &mut RuntimeBuilder);
@@ -64,10 +69,28 @@ impl RuntimeBuilder {
     }
 
     pub fn run(self, config: RuntimeConfig) -> Arc<Runtime> {
+        debug!("Spawning lump store server");
+        let lump_store = Arc::new(LumpStoreImpl::new());
+        let (lump_store_server, lump_store_client) = LumpStoreServerShared::new(lump_store, 1024);
+        tokio::spawn(async move {
+            lump_store_server.serve(true);
+        });
+
+        debug!("Spawning process store server");
+        let process_store = Arc::new(ProcessStoreImpl::new());
+        let (process_store_server, process_store_client) =
+            ProcessStoreServerShared::new(process_store, 1024);
+        tokio::spawn(async move {
+            process_store_server.serve(true);
+        });
+
         let runtime = Arc::new(Runtime {
-            peer_provider: config.peer_provider,
-            this_peer: config.this_peer,
             asset_store: self.asset_store,
+            lump_store,
+            lump_store_client,
+            process_store,
+            process_store_client,
+            config,
         });
 
         for runner in self.runners {
@@ -85,15 +108,42 @@ pub struct RuntimeConfig {
 
     /// The ID of this peer.
     pub this_peer: PeerId,
+
+    /// The [PeerInfo] for this peer.
+    pub info: PeerInfo,
 }
 
 pub struct Runtime {
+    /// The configuration of this runtime.
+    pub config: RuntimeConfig,
+
     //// The assets in this runtime.
     pub asset_store: AssetStore,
 
-    /// The provider to other peers on the network.
-    pub peer_provider: PeerProviderClient,
+    /// This runtime's lump store.
+    pub lump_store: Arc<LumpStoreImpl>,
 
-    /// The [PeerId] that this runtime represents.
-    pub this_peer: PeerId,
+    /// A clone-able client to this runtime's lump store.
+    pub lump_store_client: LumpStoreClient,
+
+    /// This runtime's process store.
+    pub process_store: Arc<ProcessStoreImpl>,
+
+    /// A clone-able client to this runtime's process store.
+    pub process_store_client: ProcessStoreClient,
+}
+
+#[async_trait]
+impl PeerApi for Runtime {
+    async fn get_info(&self) -> CallResult<PeerInfo> {
+        Ok(self.config.info.clone())
+    }
+
+    async fn get_process_store(&self) -> CallResult<ProcessStoreClient> {
+        Ok(self.process_store_client.clone())
+    }
+
+    async fn get_lump_store(&self) -> CallResult<LumpStoreClient> {
+        Ok(self.lump_store_client.clone())
+    }
 }
