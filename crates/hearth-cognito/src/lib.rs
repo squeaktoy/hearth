2086906b6c47d6120c16a657b57729bf3e93705e
marceline-cramer <cramermarceline@gmail.com>
2023-02-18 15:35:40 -0700

    cognito: add WasmProcess

--- crates/hearth-cognito/src/lib.rs
+++ crates/hearth-cognito/src/lib.rs
@@ -1,11 +1,17 @@
+use std::sync::Arc;
+
+use hearth_core::process::{Process, ProcessContext};
 use hearth_macros::impl_wasm_linker;
-use hearth_rpc::ProcessApi;
+use hearth_rpc::{remoc, ProcessInfo};
 use hearth_wasm::{GuestMemory, WasmLinker};
-use tracing::info;
-use wasmtime::{Caller, Linker};
+use remoc::rtc::async_trait;
+use tracing::{error, info};
+use wasmtime::*;
 
 /// This contains all script-accessible process-related stuff.
-pub struct Cognito {}
+pub struct Cognito {
+    ctx: ProcessContext,
+}
 
 // Should automatically generate link_print_hello_world:
 // #[impl_wasm_linker]
@@ -32,6 +38,59 @@ impl Cognito {
     // which in this example is just ().
     pub fn log_message(&self, mut memory: GuestMemory<'_>, msg_ptr: u32, msg_len: u32) {
         eprintln!("message from wasm: {}", memory.get_str(msg_ptr, msg_len));
+    }
+}
+
+struct ProcessData {
+    cognito: Cognito,
+}
+
+impl AsRef<Cognito> for ProcessData {
+    fn as_ref(&self) -> &Cognito {
+        &self.cognito
+    }
+}
+
+struct WasmProcess {
+    engine: Arc<Engine>,
+    linker: Arc<Linker<ProcessData>>,
+    module: Arc<Module>,
+}
+
+#[async_trait]
+impl Process for WasmProcess {
+    fn get_info(&self) -> ProcessInfo {
+        ProcessInfo {}
+    }
+
+    async fn run(&mut self, ctx: ProcessContext) {
+        // TODO log using the process log instead of tracing?
+        let cognito = Cognito { ctx };
+        let data = ProcessData { cognito };
+        let mut store = Store::new(&self.engine, data);
+        let instance = match self
+            .linker
+            .instantiate_async(&mut store, &self.module)
+            .await
+        {
+            Ok(instance) => instance,
+            Err(err) => {
+                error!("Failed to instantiate WasmProcess: {:?}", err);
+                return;
+            }
+        };
+
+        // TODO better wasm invocation?
+        match instance.get_typed_func::<(), ()>(&mut store, "run") {
+            Ok(run) => {
+                if let Err(err) = run.call_async(&mut store, ()).await {
+                    error!("Wasm run error: {:?}", err);
+                }
+            }
+            Err(err) => {
+                error!("Couldn't find run function: {:?}", err);
+            }
+        }
     }
 }
 
@@ -68,7 +127,7 @@ mod tests {
         let mut config = Config::new();
         config.async_support(true);
         let engine = Engine::new(&config).unwrap();
-        let mut linker: wasmtime::Linker<MockStructure> = Linker::new(&engine);
+        let mut linker: Linker<MockStructure> = Linker::new(&engine);
         let mut store = Store::new(&engine, MockStructure::default());
         Cognito::add_to_linker(&mut linker);
         (linker, store)
