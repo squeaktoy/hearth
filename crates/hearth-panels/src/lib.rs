63f2a3315ed9554b09d6822dd4849feb83e37d06
marceline-cramer <cramermarceline@gmail.com>
2023-03-13 13:40:56 -0600

    panels: add deps and stub PanelsPlugin

--- crates/hearth-panels/src/lib.rs
+++ crates/hearth-panels/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Copyright (c) 2023 the Hearth contributors.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This file is part of Hearth.
+//
+// Hearth is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Hearth is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Hearth. If not, see <https://www.gnu.org/licenses/>.
+
+use std::sync::Arc;
+
+use hearth_rpc::remoc::rtc::async_trait;
+use hearth_core::runtime::{Plugin, Runtime, RuntimeBuilder};
+
+pub struct PanelsPlugin {}
+
+#[async_trait]
+impl Plugin for PanelsPlugin {
+    fn build(&mut self, builder: &mut RuntimeBuilder) {}
+
+    async fn run(&mut self, runtime: Arc<Runtime>) {}
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl PanelsPlugin {
+    pub fn new() -> Self {
+        Self {}
     }
 }
