c40138758759aeb76d014bc1f91b45fcc2426e4b
marceline-cramer <cramermarceline@gmail.com>
2023-03-14 13:35:24 -0600

    panels: add services and PanelStore + focus unit test

--- crates/hearth-panels/src/lib.rs
+++ crates/hearth-panels/src/lib.rs
@@ -18,14 +18,33 @@
 
 use std::sync::Arc;
 
-use hearth_rpc::remoc::rtc::async_trait;
+use hearth_core::process::{Process, ProcessContext};
+use hearth_core::pubsub::PublisherProcess;
 use hearth_core::runtime::{Plugin, Runtime, RuntimeBuilder};
+use hearth_core::tokio::sync::mpsc;
+use hearth_rpc::hearth_types::panels::*;
+use hearth_rpc::remoc::rtc::async_trait;
+use hearth_rpc::ProcessInfo;
+use slab::Slab;
 
+/// Plugin to add paneling support to a Hearth runtime.
+///
+/// Adds the [PanelControlService] and [AmbientPanelService] services.
 pub struct PanelsPlugin {}
 
 #[async_trait]
 impl Plugin for PanelsPlugin {
-    fn build(&mut self, builder: &mut RuntimeBuilder) {}
+    fn build(&mut self, builder: &mut RuntimeBuilder) {
+        builder.add_service(
+            "hearth.panels.PanelControlService".into(),
+            PanelControlService::new(),
+        );
+
+        /*builder.add_service(
+            "hearth.panels.AmbientPanelService".into(),
+            AmbientPanelService::new(),
+        );*/
+    }
 
     async fn run(&mut self, runtime: Arc<Runtime>) {}
 }
@@ -33,5 +52,137 @@ impl Plugin for PanelsPlugin {
 impl PanelsPlugin {
     pub fn new() -> Self {
         Self {}
+    }
+}
+
+/// `hearth.panels.PanelControlService`: Controls panels.
+pub struct PanelControlService {}
+
+#[async_trait]
+impl Process for PanelControlService {
+    fn get_info(&self) -> ProcessInfo {
+        ProcessInfo {}
+    }
+
+    async fn run(&mut self, mut ctx: ProcessContext) {
+        ctx.join().await;
+    }
+}
+
+impl PanelControlService {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+/// `hearth.panels.AmbientPanelService`: Publisher for panel events addressed
+/// to the ambient panel.
+pub type AmbientPanelService = PublisherProcess<PanelEvent>;
+
+/// Core data structure for panel management.
+pub struct PanelStore {
+    /// The pool of [PanelWrappers][PanelWrapper] to active panels.
+    pub panels: Slab<PanelWrapper>,
+
+    /// The currently focused panel.
+    pub focused: Option<usize>,
+}
+
+impl PanelStore {
+    /// Creates an empty [PanelStore].
+    pub fn new() -> Self {
+        Self {
+            panels: Default::default(),
+            focused: None,
+        }
+    }
+
+    /// Adds a new panel to the store. Returns its ID.
+    pub fn add_panel(&mut self, wrapper: PanelWrapper) -> usize {
+        self.panels.insert(wrapper)
+    }
+
+    /// Changes panel focus to a new panel.
+    ///
+    /// Returns the ID of the last focused panel. Does nothing and returns
+    /// `None` if the new panel ID is invalid.
+    pub fn focus(&mut self, panel: usize) -> Option<usize> {
+        if !self.panel_send(panel, PanelEvent::Focus(true)) {
+            return None;
+        }
+
+        if let Some(last) = self.focused.replace(panel) {
+            // self.focused should never point to an invalid panel
+            self.expect_send(last, PanelEvent::Focus(false));
+            Some(last)
+        } else {
+            None
+        }
+    }
+
+    /// Safely sends a panel event to a panel, returning true if successful.
+    fn panel_send(&mut self, panel: usize, message: PanelEvent) -> bool {
+        if let Some(wrapper) = self.panels.get(panel) {
+            let result = wrapper.event_tx.send(message);
+            if result.is_err() {
+                self.panels.remove(panel);
+                false // remote hung up
+            } else {
+                true // successfully sent the message
+            }
+        } else {
+            false // no panel was found
+        }
+    }
+
+    /// Gets a panel wrapper but panic if the ID was not found.
+    fn expect_panel(&self, panel: usize) -> &PanelWrapper {
+        match self.panels.get(panel) {
+            Some(wrapper) => wrapper,
+            None => panic!("Expected panel ID {} to be valid", panel),
+        }
+    }
+
+    /// Sends a message to a panel, returning true if successful, but panics if
+    /// the ID was not found.
+    fn expect_send(&mut self, panel: usize, message: PanelEvent) -> bool {
+        let result = self.expect_panel(panel).event_tx.send(message);
+        if result.is_err() {
+            self.panels.remove(panel);
+            false // remote hung up
+        } else {
+            true // successfully sent the message
+        }
+    }
+}
+
+/// Handle to a remote panel implementation.
+pub struct PanelWrapper {
+    /// The sender to send messages.
+    pub event_tx: mpsc::UnboundedSender<PanelEvent>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn add_passthru(store: &mut PanelStore) -> (usize, mpsc::UnboundedReceiver<PanelEvent>) {
+        let (event_tx, event_rx) = mpsc::unbounded_channel();
+        let wrapper = PanelWrapper { event_tx };
+        let id = store.add_panel(wrapper);
+        (id, event_rx)
+    }
+
+    #[test]
+    fn focus() {
+        let mut store = PanelStore::new();
+        let (p1_id, mut p1_rx) = add_passthru(&mut store);
+        let (p2_id, mut p2_rx) = add_passthru(&mut store);
+        assert_eq!(store.focus(42), None);
+        assert_eq!(store.focus(p1_id), None);
+        assert_eq!(p1_rx.try_recv().unwrap(), PanelEvent::Focus(true));
+        assert_eq!(store.focus(p2_id), Some(p1_id));
+        assert_eq!(p1_rx.try_recv().unwrap(), PanelEvent::Focus(false));
+        assert_eq!(p2_rx.try_recv().unwrap(), PanelEvent::Focus(true));
     }
 }
