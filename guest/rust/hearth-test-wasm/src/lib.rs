6acaa7ee57c5267c45873d1a9c477fc0b4c2187c
marceline-cramer <cramermarceline@gmail.com>
2023-06-15 22:32:50 -0600

    test-wasm: update to new APIs

--- guest/rust/hearth-test-wasm/src/lib.rs
+++ guest/rust/hearth-test-wasm/src/lib.rs
@@ -1,24 +1,22 @@
-use hearth_guest::{wasm::WasmSpawnInfo, LocalProcessId, ProcessId};
+use hearth_guest::{wasm::WasmSpawnInfo, Message, Process};
 
-fn get_spawner() -> ProcessId {
-    let this_peer = hearth_guest::this_pid().split().0;
-    hearth_guest::service_lookup(this_peer, "hearth.cognito.WasmProcessSpawner")
-        .expect("Couldn't find Wasm spawner service")
+fn get_spawner() -> Process {
+    Process::get_service("hearth.cognito.WasmProcessSpawner")
+        .expect("couldn't find Wasm spawner service")
 }
 
-fn send<T: serde::Serialize>(pid: ProcessId, data: &T) {
-    let msg = serde_json::to_vec(data).unwrap();
-    hearth_guest::send(pid, &msg);
+fn send<T: serde::Serialize>(dst: &Process, data: &T) {
+    let msg = serde_json::to_string(data).unwrap();
+    dst.send(&msg.into_bytes(), &[&hearth_guest::SELF]);
 }
 
-fn recv<T: for<'a> serde::Deserialize<'a>>() -> (ProcessId, T) {
-    let msg = hearth_guest::recv();
-    let sender = msg.get_sender();
-    let data = serde_json::from_slice(&msg.get_data()).unwrap();
-    (sender, data)
+fn recv<T: for<'a> serde::Deserialize<'a>>() -> (Vec<Process>, T) {
+    let msg = Message::recv();
+    let data = serde_json::from_slice(&msg.data).unwrap();
+    (msg.caps, data)
 }
 
-fn spawn(spawner: ProcessId, cb: fn()) -> ProcessId {
+fn spawn(spawner: &Process, cb: fn()) -> Process {
     send(
         spawner,
         &WasmSpawnInfo {
@@ -27,16 +25,15 @@ fn spawn(spawner: ProcessId, cb: fn()) -> ProcessId {
         },
     );
 
-    let msg = String::from_utf8(hearth_guest::recv().get_data()).unwrap();
-    let local_pid: u32 = msg.parse().unwrap();
-    ProcessId::from_peer_process(spawner.split().0, LocalProcessId(local_pid))
+    let mut msg = Message::recv();
+    msg.caps.remove(0)
 }
 
 #[no_mangle]
 pub extern "C" fn run() {
     let spawner = get_spawner();
-    let input: Vec<u64> = (0..100000).into_iter().rev().collect();
-    merge_sort(spawner, input);
+    let input: Vec<u64> = (0..10000000).into_iter().rev().collect();
+    merge_sort(&spawner, input);
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -58,23 +55,25 @@ fn bubble_sort(input: &mut Vec<u64>) {
 }
 
 fn merge_sort_outer() {
+    let (mut caps, input) = recv::<Data>();
+    let parent = caps.remove(0);
     let spawner = get_spawner();
-    let (parent, input) = recv::<Data>();
-    let result = merge_sort(spawner, input.inner);
-    send(parent, &Data { inner: result });
+    let result = merge_sort(&spawner, input.inner);
+    send(&parent, &Data { inner: result });
 }
 
-fn merge_sort(spawner: ProcessId, mut input: Vec<u64>) -> Vec<u64> {
+fn merge_sort(spawner: &Process, mut input: Vec<u64>) -> Vec<u64> {
     if input.len() > 32 {
         let a_pid = spawn(spawner, merge_sort_outer);
         let b_pid = spawn(spawner, merge_sort_outer);
 
         let (a, b) = input.split_at(input.len() / 2);
-        send(a_pid, &Data { inner: a.to_vec() });
-        send(b_pid, &Data { inner: b.to_vec() });
+        send(&a_pid, &Data { inner: a.to_vec() });
+        send(&b_pid, &Data { inner: b.to_vec() });
 
-        let (_sender, a_sorted) = recv::<Data>();
-        let (_sender, b_sorted) = recv::<Data>();
+        let (_caps, a_sorted) = recv::<Data>();
+        let (_caps, b_sorted) = recv::<Data>();
+
         merge(a_sorted.inner, b_sorted.inner)
     } else {
         bubble_sort(&mut input);
