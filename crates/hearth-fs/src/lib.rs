ee225d72b888b8d37777338c7bba34ca66b011ac
marceline-cramer <cramermarceline@gmail.com>
2023-08-22 13:15:33 -0600

    fs: use RPC helpers

--- crates/hearth-fs/src/lib.rs
+++ crates/hearth-fs/src/lib.rs
@@ -16,129 +16,74 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Hearth. If not, see <https://www.gnu.org/licenses/>.
 
-use std::{
-    path::{Path, PathBuf},
-    sync::Arc,
-};
+use std::path::PathBuf;
 
 use hearth_core::{
-    hearth_types::{fs::*, Flags, ProcessLogLevel},
-    lump::LumpStoreImpl,
-    process::{
-        context::{ContextMessage, ContextSignal},
-        factory::{ProcessInfo, ProcessLogEvent},
-        Process,
-    },
-    runtime::{Plugin, RuntimeBuilder},
-    tracing::warn,
+    anyhow, async_trait,
+    hearth_types::fs::*,
+    utils::{RequestInfo, RequestResponseService},
 };
-
-async fn on_request(root: &Path, request: Request, lumps: &LumpStoreImpl) -> Response {
-    let target = PathBuf::try_from(request.target).map_err(|_| Error::InvalidTarget)?;
-    let mut path = root.to_path_buf();
-    for component in target.components() {
-        match component {
-            std::path::Component::Normal(normal) => path.push(normal),
-            _ => return Err(Error::DirectoryTraversal),
-        }
-    }
-
-    match request.kind {
-        RequestKind::Get => {
-            let contents = match std::fs::read(path) {
-                Ok(contents) => contents,
-                Err(_) => todo!(),
-            };
-
-            let lump = lumps.add_lump(contents.into()).await;
-            Ok(Success::Get(lump))
-        }
-        RequestKind::List => {
-            let dirs = match std::fs::read_dir(path) {
-                Ok(dirs) => dirs,
-                Err(_) => todo!(),
-            };
-
-            let dirs: Vec<_> = dirs
-                .into_iter()
-                .map(|dir| {
-                    let dir = dir.unwrap();
-
-                    FileInfo {
-                        name: dir.file_name().to_string_lossy().to_string(),
-                    }
-                })
-                .collect();
-
-            Ok(Success::List(dirs))
-        }
-    }
-}
 
 pub struct FsPlugin {
     root: PathBuf,
 }
 
-impl Plugin for FsPlugin {
-    fn finish(self, builder: &mut RuntimeBuilder) {
-        builder.add_service(
-            "hearth.fs.Filesystem".into(),
-            ProcessInfo {},
-            Flags::SEND,
-            move |runtime, process| {
-                hearth_core::tokio::spawn(async move {
-                    self.serve(process, runtime.lump_store.clone()).await;
-                });
-            },
-        );
+#[async_trait]
+impl RequestResponseService for FsPlugin {
+    const NAME: &'static str = "hearth.fs.Filesystem";
+    type Request = Request;
+    type Response = Response;
+
+    async fn on_request(&mut self, request: RequestInfo<'_, Request>) -> anyhow::Result<Response> {
+        let target = match PathBuf::try_from(request.data.target) {
+            Ok(target) => target,
+            Err(_) => return Ok(Err(Error::InvalidTarget)),
+        };
+
+        let mut path = self.root.to_path_buf();
+        for component in target.components() {
+            match component {
+                std::path::Component::Normal(normal) => path.push(normal),
+                _ => return Ok(Err(Error::DirectoryTraversal)),
+            }
+        }
+
+        match request.data.kind {
+            RequestKind::Get => {
+                let contents = match std::fs::read(path) {
+                    Ok(contents) => contents,
+                    Err(_) => todo!(),
+                };
+
+                let lump = request.runtime.lump_store.add_lump(contents.into()).await;
+
+                Ok(Ok(Success::Get(lump)))
+            }
+            RequestKind::List => {
+                let dirs = match std::fs::read_dir(path) {
+                    Ok(dirs) => dirs,
+                    Err(_) => todo!(),
+                };
+
+                let dirs: Vec<_> = dirs
+                    .into_iter()
+                    .map(|dir| {
+                        let dir = dir.unwrap();
+
+                        FileInfo {
+                            name: dir.file_name().to_string_lossy().to_string(),
+                        }
+                    })
+                    .collect();
+
+                Ok(Ok(Success::List(dirs)))
+            }
+        }
     }
 }
 
 impl FsPlugin {
     pub fn new(root: PathBuf) -> Self {
         Self { root }
-    }
-
-    async fn serve(&self, mut ctx: Process, lumps: Arc<LumpStoreImpl>) {
-        while let Some(signal) = ctx.recv().await {
-            let ContextSignal::Message(message) = signal else {
-                panic!("expected message signal; got {:?}", signal);
-            };
-
-            // TODO share this code with hearth-cognito in hearth-core
-            let request: Request = match serde_json::from_slice(&message.data) {
-                Ok(message) => message,
-                Err(err) => {
-                    ctx.log(ProcessLogEvent {
-                        level: ProcessLogLevel::Error,
-                        module: "WasmProcessSpawner".to_string(),
-                        content: format!("Failed to parse WasmSpawnInfo: {:?}", err),
-                    });
-
-                    warn!("Failed to parse WasmSpawnInfo: {:?}", err);
-
-                    continue;
-                }
-            };
-
-            let response = on_request(&self.root, request, lumps.as_ref()).await;
-            let response = serde_json::to_vec(&response).unwrap();
-            let Some(reply) = message.caps.first().copied() else {
-                continue;
-            };
-
-            ctx.send(
-                reply,
-                ContextMessage {
-                    data: response,
-                    caps: vec![],
-                },
-            )
-            .unwrap();
-
-            for unused in message.caps {
-                ctx.delete_capability(unused).unwrap();
-            }
-        }
     }
 }
