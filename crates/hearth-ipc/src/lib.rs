ee4bcc69f34dd8cedc65f54b669cb1ead0ed5122
marceline-cramer <cramermarceline@gmail.com>
2023-08-26 13:51:32 -0600

    ipc: remove daemon-only code

--- crates/hearth-ipc/src/lib.rs
+++ crates/hearth-ipc/src/lib.rs
@@ -16,10 +16,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Hearth. If not, see <https://www.gnu.org/licenses/>.
 
-use std::ops::{Deref, DerefMut};
 use std::path::PathBuf;
 
-use tokio::net::{UnixListener, UnixStream};
+use tokio::net::UnixStream;
 
 /// Returns the path of the Hearth IPC socket.
 ///
@@ -53,88 +52,6 @@ pub fn get_socket_path() -> Option<PathBuf> {
     }
 
     None
-}
-
-pub struct Listener {
-    pub uds: UnixListener,
-    pub path: PathBuf,
-}
-
-impl Drop for Listener {
-    fn drop(&mut self) {
-        match std::fs::remove_file(&self.path) {
-            Ok(_) => {}
-            Err(e) => tracing::error!("Could not delete UnixListener {:?}", e),
-        }
-    }
-}
-
-impl Deref for Listener {
-    type Target = UnixListener;
-
-    fn deref(&self) -> &UnixListener {
-        &self.uds
-    }
-}
-
-impl DerefMut for Listener {
-    fn deref_mut(&mut self) -> &mut UnixListener {
-        &mut self.uds
-    }
-}
-
-impl Listener {
-    pub async fn new() -> std::io::Result<Self> {
-        use std::io::{Error, ErrorKind};
-
-        let sock_path = match get_socket_path() {
-            Some(p) => p,
-            None => {
-                let kind = ErrorKind::NotFound;
-                let msg = "Failed to find a socket path";
-                tracing::error!(msg);
-                return Err(Error::new(kind, msg));
-            }
-        };
-
-        match UnixStream::connect(&sock_path).await {
-            Ok(_) => {
-                tracing::warn!(
-                    "Socket is already in use. Another instance of Hearth may be running."
-                );
-                let kind = ErrorKind::AddrInUse;
-                let error = Error::new(kind, "Socket is already in use.");
-                return Err(error);
-            }
-            Err(ref err) if err.kind() == ErrorKind::ConnectionRefused => {
-                tracing::warn!("Found leftover socket; removing.");
-                std::fs::remove_file(&sock_path)?;
-            }
-            Err(ref err) if err.kind() == ErrorKind::NotFound => {}
-            Err(err) => return Err(err),
-        }
-
-        tracing::info!("Making socket at: {:?}", sock_path);
-        let uds = UnixListener::bind(&sock_path)?;
-        let path = sock_path.to_path_buf();
-        Ok(Self { uds, path })
-    }
-
-    /// Spawns a Tokio thread to respond to connections on the domain socket.
-    pub fn listen(self) {
-        tokio::spawn(async move {
-            loop {
-                match self.accept().await {
-                    Ok((_socket, addr)) => {
-                        tracing::debug!("Accepting IPC connection from {:?}", addr);
-                    }
-                    Err(err) => {
-                        tracing::error!("IPC listen error: {:?}", err);
-                    }
-                }
-            }
-        });
-    }
 }
 
 /// Connects to the Hearth daemon and returns a [UnixStream].
