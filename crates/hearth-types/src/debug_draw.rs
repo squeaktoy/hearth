915f3314ae735e1e1714bc2b6bdacd554d214aa4
marceline-cramer <cramermarceline@gmail.com>
2023-10-24 16:30:18 -0600

    types: add Color type and use in debug draw

--- crates/hearth-types/src/debug_draw.rs
+++ crates/hearth-types/src/debug_draw.rs
@@ -19,10 +19,15 @@
 use glam::Vec3;
 use serde::{Deserialize, Serialize};
 
+use crate::Color;
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct DebugDrawVertex {
+    /// The position of this vertex in world space.
     pub position: Vec3,
-    pub color: [u8; 3],
+
+    /// The color of this vertex. Alpha is ignored and fixed to opaque.
+    pub color: Color,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
