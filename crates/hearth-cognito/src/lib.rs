57d54edfc35e1be8e09946f9291e196229a22f1b
marceline-cramer <cramermarceline@gmail.com>
2023-11-12 11:06:21 -0700

    cognito: migrate hearth-wasm types

--- crates/hearth-cognito/src/lib.rs
+++ crates/hearth-cognito/src/lib.rs
@@ -29,10 +29,81 @@ use hearth_core::{cargo_process_metadata, tokio, utils::*};
 use hearth_macros::impl_wasm_linker;
 use hearth_types::wasm::WasmSpawnInfo;
 use hearth_types::{LumpId, SignalKind};
-use hearth_wasm::{GuestMemory, WasmLinker};
 use slab::Slab;
 use tracing::{debug, error};
 use wasmtime::{Caller, Config, Engine, Linker, Module, Store, UpdateDeadline};
+
+pub trait WasmLinker<T: AsMut<Self>> {
+    fn add_to_linker(linker: &mut wasmtime::Linker<T>);
+}
+
+pub struct GuestMemory<'a> {
+    pub bytes: &'a mut [u8],
+}
+
+impl<'a> GuestMemory<'a> {
+    pub fn from_caller<T>(caller: &mut Caller<'a, T>) -> Result<Self> {
+        let memory = caller
+            .get_export("memory")
+            .ok_or_else(|| anyhow!("Caller does not export memory"))?
+            .into_memory()
+            .ok_or_else(|| anyhow!("Caller 'memory' export is not a memory"))?;
+        let data_ptr = memory.data_ptr(&caller);
+        let data_size = memory.data_size(&caller);
+        let bytes = unsafe { std::slice::from_raw_parts_mut(data_ptr, data_size) };
+        Ok(Self { bytes })
+    }
+
+    pub fn get_str(&self, ptr: u32, len: u32) -> Result<&'a mut str> {
+        let memory = self.get_slice(ptr, len)?;
+        std::str::from_utf8_mut(memory)
+            .with_context(|| format!("GuestMemory::get_str({}, {})", ptr, len))
+    }
+
+    pub fn get_slice(&self, ptr: u32, len: u32) -> Result<&'a mut [u8]> {
+        let ptr = ptr as usize;
+        let len = len as usize;
+        if ptr + len > self.bytes.len() {
+            Err(anyhow!(
+                "GuestMemory::get_slice({}, {}) is out-of-bounds",
+                ptr,
+                len
+            ))
+        } else {
+            unsafe {
+                let ptr = self.bytes.as_ptr().add(ptr) as *mut u8;
+                Ok(std::slice::from_raw_parts_mut(ptr, len))
+            }
+        }
+    }
+
+    pub fn get_memory_ref<T: bytemuck::Pod>(&self, ptr: u32) -> Result<&'a mut T> {
+        let len = std::mem::size_of::<T>() as u32;
+        let bytes = self.get_slice(ptr, len)?;
+        bytemuck::try_from_bytes_mut(bytes).map_err(|err| {
+            anyhow!(
+                "GuestMemory::get_memory_ref<{}>({}) failed: {:?}",
+                std::any::type_name::<T>(),
+                ptr,
+                err
+            )
+        })
+    }
+
+    pub fn get_memory_slice<T: bytemuck::Pod>(&self, ptr: u32, num: u32) -> Result<&'a mut [T]> {
+        let len = num * std::mem::size_of::<T>() as u32;
+        let bytes = self.get_slice(ptr, len)?;
+        bytemuck::try_cast_slice_mut(bytes).map_err(|err| {
+            anyhow!(
+                "GuestMemory::get_memory_slice<{}>({}, {}) failed: {:?}",
+                std::any::type_name::<T>(),
+                ptr,
+                num,
+                err
+            )
+        })
+    }
+}
 
 /// Implements the `hearth::log` ABI module.
 pub struct LogAbi {
