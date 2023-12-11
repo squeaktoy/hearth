6a7b598f329a262cdf3a240654b8433cde60e17f
airidaceae <78971823+airidaceae@users.noreply.github.com>
2023-12-11 16:20:24 -0500

    kindling-init: use kindling-host for fs operations

--- kindling/init/src/lib.rs
+++ kindling/init/src/lib.rs
@@ -16,8 +16,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Hearth. If not, see <https://www.gnu.org/licenses/>.
 
-use hearth_guest::*;
-use kindling_host::{registry::REGISTRY, wasm::spawn_mod};
+use kindling_host::{fs::*, wasm::spawn_mod};
 
 macro_rules! log {
     ($level:expr, $($arg:tt)*) => {
@@ -38,61 +37,13 @@ macro_rules! info {
 #[no_mangle]
 pub extern "C" fn run() {
     hearth_guest::log(hearth_guest::ProcessLogLevel::Info, "init", "Hello world!");
-    let fs = REGISTRY
-        .get_service("hearth.fs.Filesystem")
-        .expect("Filesystem service not found");
+    let fs = Filesystem::new();
     let search_dir = "init";
-    for file in list_files(&fs, search_dir) {
+    for file in fs.list_files(search_dir).unwrap() {
         info!("file: {}", file.name);
-        let lump = get_file(&fs, &format!("init/{}/service.wasm", file.name));
+        let lump = fs
+            .get_file(&format!("init/{}/service.wasm", file.name))
+            .unwrap();
         spawn_mod(lump, None).unwrap();
     }
-}
-
-//TODO break these file system operations into a common crate
-fn request_fs(fs: &Capability, request: fs::Request) -> fs::Success {
-    log!(ProcessLogLevel::Debug, "making fs request: {:?}", request);
-    let reply = Mailbox::new();
-    let reply_cap = reply.make_capability(Permissions::SEND);
-    fs.send_json(&request, &[&reply_cap]);
-    let (response, _caps) = reply.recv_json::<fs::Response>();
-    response.unwrap()
-}
-
-fn get_file(fs: &Capability, path: &str) -> LumpId {
-    let success = request_fs(
-        fs,
-        fs::Request {
-            target: path.to_string(),
-            kind: fs::RequestKind::Get,
-        },
-    );
-
-    let fs::Success::Get(lump) = success else {
-        panic!("expected Success::Get, got {:?}", success)
-    };
-
-    lump
-}
-
-fn read_file(fs: &Capability, path: &str) -> Vec<u8> {
-    let lump = get_file(fs, path);
-    let lump = Lump::load_by_id(&lump);
-    lump.get_data()
-}
-
-fn list_files(fs: &Capability, path: &str) -> Vec<fs::FileInfo> {
-    let success = request_fs(
-        fs,
-        fs::Request {
-            target: path.to_string(),
-            kind: fs::RequestKind::List,
-        },
-    );
-
-    let fs::Success::List(files) = success else {
-        panic!("expected Success::List, got {:?}", success)
-    };
-
-    files
 }
