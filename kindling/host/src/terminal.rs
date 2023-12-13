e3d76099e957fa00a727d3321feebe0392825e2d
airidaceae <78971823+airidaceae@users.noreply.github.com>
2023-12-13 00:08:10 -0500

    kindling-host: hide terminal factory

--- kindling/host/src/terminal.rs
+++ kindling/host/src/terminal.rs
@@ -22,43 +22,42 @@ use hearth_guest::terminal::*;
 
 lazy_static::lazy_static! {
     /// A lazily-initialized handle to the terminal factory service.
-    pub static ref TERMINAL_FACTORY: RequestResponse<FactoryRequest, FactoryResponse> = {
-        RequestResponse::new(registry::REGISTRY.get_service("hearth.canvas.TerminalFactory").unwrap())    };
-}
-
-impl TERMINAL_FACTORY {
-    /// Spawns a new Terminal.
-    pub fn spawn_canvas(&self, request: FactoryRequest) -> TerminalWrapper {
-        let resp = self.request(request, &[]);
-        let success = resp.0.unwrap();
-        match success {
-            FactorySuccess::Terminal => TerminalWrapper {
-                cap: resp.1.get(0).unwrap().clone(),
-            },
-        }
-    }
+    static ref TERMINAL_FACTORY: RequestResponse<FactoryRequest, FactoryResponse> = {
+        RequestResponse::new(registry::REGISTRY.get_service("hearth.canvas.TerminalFactory").unwrap())
+    };
 }
 
 /// A wrapper around the Terminal Capability.
-pub struct TerminalWrapper {
+pub struct Terminal {
     cap: Capability,
 }
 
 // This may change, depends on https://github.com/hearth-rs/hearth/issues/189
-impl Drop for TerminalWrapper {
+impl Drop for Terminal {
     fn drop(&mut self) {
         self.cap.send_json(&TerminalUpdate::Quit, &[]);
     }
 }
 
-impl TerminalWrapper {
-    /// Send input to this terminal
+impl Terminal {
+    /// Creates a new terminal with the given TerminalState.
+    pub fn new(state: TerminalState) -> Self {
+        let resp = TERMINAL_FACTORY.request(FactoryRequest::CreateTerminal(state), &[]);
+        let success = resp.0.unwrap();
+        match success {
+            FactorySuccess::Terminal => Terminal {
+                cap: resp.1.get(0).unwrap().clone(),
+            },
+        }
+    }
+
+    /// Send input to this terminal.
     pub fn input(&self, input: String) {
         self.cap.send_json(&TerminalUpdate::Input(input), &[])
     }
 
     /// Update the state of this terminal.
-    pub fn state(&self, state: TerminalState) {
+    pub fn update(&self, state: TerminalState) {
         self.cap.send_json(&TerminalUpdate::State(state), &[])
     }
 }
