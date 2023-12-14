6a725234a40042abdbc754ad82ab8596e4430640
airidaceae <78971823+airidaceae@users.noreply.github.com>
2023-12-14 11:17:14 -0500

    kindling-debug-grid: add closure to make vertices

--- kindling/services/debug-grid/src/lib.rs
+++ kindling/services/debug-grid/src/lib.rs
@@ -24,31 +24,23 @@ pub extern "C" fn run() {
     let size = 15;
     let color = Color::from_rgb(0x6a, 0xf5, 0xfc);
     let grid_to_pos = |x: i32, y: i32| vec3(x as f32 * 5.0, -8.0, y as f32 * 5.0);
+    let vertex = |x, y, color| -> DebugDrawVertex {
+        DebugDrawVertex {
+            position: grid_to_pos(x, y),
+            color,
+        }
+    };
 
     let mut vertices = Vec::new();
 
     for x in -size..=size {
-        vertices.push(DebugDrawVertex {
-            position: grid_to_pos(x, -size),
-            color,
-        });
-
-        vertices.push(DebugDrawVertex {
-            position: grid_to_pos(x, size),
-            color,
-        });
+        vertices.push(vertex(x, -size, color));
+        vertices.push(vertex(x, size, color));
     }
 
     for y in -size..=size {
-        vertices.push(DebugDrawVertex {
-            position: grid_to_pos(-size, y),
-            color,
-        });
-
-        vertices.push(DebugDrawVertex {
-            position: grid_to_pos(size, y),
-            color,
-        });
+        vertices.push(vertex(-size, y, color));
+        vertices.push(vertex(size, y, color));
     }
 
     let dd = DebugDraw::new();
