4cf4a2733038df56c378bb7bb87de2484cb5d571
airidaceae <78971823+airidaceae@users.noreply.github.com>
2023-12-14 11:07:51 -0500

    kindling-debug-grid: use host library

--- kindling/services/debug-grid/src/lib.rs
+++ kindling/services/debug-grid/src/lib.rs
@@ -16,13 +16,11 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Hearth. If not, see <https://www.gnu.org/licenses/>.
 
-use glam::vec3;
-use hearth_guest::{debug_draw::*, *};
+use hearth_guest::{debug_draw::*, Color};
+use kindling_host::prelude::{glam::vec3, DebugDraw};
 
 #[no_mangle]
 pub extern "C" fn run() {
-    let dd_factory = REGISTRY.get_service("hearth.DebugDrawFactory").unwrap();
-
     let size = 15;
     let color = Color::from_rgb(0x6a, 0xf5, 0xfc);
     let grid_to_pos = |x: i32, y: i32| vec3(x as f32 * 5.0, -8.0, y as f32 * 5.0);
@@ -53,17 +51,10 @@ pub extern "C" fn run() {
         });
     }
 
-    let reply = Mailbox::new();
-    let reply_cap = reply.make_capability(Permissions::SEND);
-    dd_factory.send_json(&(), &[&reply_cap]);
-    let ((), caps) = reply.recv_json();
-    let dd = caps.get(0).unwrap().clone();
-
-    dd.send_json(
-        &DebugDrawUpdate::Contents(DebugDrawMesh {
-            indices: (0..(vertices.len() as u32)).collect(),
-            vertices,
-        }),
-        &[],
-    );
+    let dd = DebugDraw::new();
+    dd.update(DebugDrawMesh {
+        indices: (0..vertices.len() as u32).collect(),
+        vertices,
+    });
+    std::mem::forget(dd);
 }
