8a793398b6ef4e885f9a1b34af4120c3933ec689
airidaceae <78971823+airidaceae@users.noreply.github.com>
2023-12-27 17:21:10 -0500

    kindling-utils: refactor registry API

--- kindling/utils/src/registry.rs
+++ kindling/utils/src/registry.rs
@@ -22,7 +22,7 @@ use hearth_guest::{
     registry::{RegistryRequest, RegistryResponse},
     Capability, PARENT,
 };
-use kindling_host::prelude::*;
+use kindling_host::{prelude::*, registry::Registry};
 use serde::{Deserialize, Serialize};
 
 // TODO: Break out to schema crate
@@ -31,30 +31,31 @@ pub struct RegistryConfig {
     pub service_names: Vec<String>,
 }
 
-pub struct Registry {
-    cap: Capability,
+pub struct RegistryServer {
+    services: HashMap<String, Capability>,
 }
 
-impl Registry {
+impl RegistryServer {
     /// Spawn a new immutable registry.
-    pub fn spawn(services: Vec<(String, Capability)>) -> Self {
+    pub fn spawn(services: Vec<(String, Capability)>) -> Registry {
         let (service_names, caps): (Vec<String>, Vec<Capability>) = services.into_iter().unzip();
         let caps: Vec<&Capability> = caps.iter().collect();
         let config = RegistryConfig { service_names };
-        let registry = spawn_fn(Self::on_spawn, None);
+        let registry = spawn_fn(Self::init, None);
         registry.send_json(&config, &caps);
-        Registry { cap: registry }
+        RequestResponse::new(registry)
     }
 
-    fn on_spawn() {
+    fn init() {
         let (config, service_list) = PARENT.recv_json::<RegistryConfig>();
 
         // Hashmap that maps the service names to their capabilities
         let mut services = HashMap::new();
         for (cap, name) in service_list.iter().zip(config.service_names) {
             info!("now serving {:?}", name);
-            services.insert(name, cap);
+            services.insert(name, cap.clone());
         }
+        let registry = RegistryServer { services };
 
         loop {
             let (request, caps) = PARENT.recv_json::<RegistryRequest>();
@@ -62,28 +63,29 @@ impl Registry {
                 debug!("Request did not contain a capability");
                 continue;
             };
-
-            let mut response_cap = vec![];
-            use RegistryRequest::*;
-            let response = match request {
-                Get { name } => match services.get(&name) {
-                    Some(service) => {
-                        response_cap.push(*service);
-                        RegistryResponse::Get(true)
-                    }
-                    None => {
-                        info!("Requested service \"{name}\" not found");
-                        RegistryResponse::Get(false)
-                    }
-                },
-                Register { .. } => {
-                    debug!("Attempted to register on an immutable registry");
-                    RegistryResponse::Register(None)
-                }
-                List => RegistryResponse::List(services.keys().map(|k| k.to_string()).collect()),
-            };
-
+            let (response, response_cap) = registry.on_request(request);
             reply.send_json(&response, &response_cap)
+        }
+    }
+
+    fn on_request(&self, request: RegistryRequest) -> (RegistryResponse, Vec<&Capability>) {
+        use RegistryRequest::*;
+        match request {
+            Get { name } => match self.services.get(&name) {
+                Some(service) => (RegistryResponse::Get(true), vec![service]),
+                None => {
+                    info!("Requested service \"{name}\" not found");
+                    (RegistryResponse::Get(false), vec![])
+                }
+            },
+            Register { .. } => {
+                debug!("Attempted to register on an immutable registry");
+                (RegistryResponse::Register(None), vec![])
+            }
+            List => (
+                RegistryResponse::List(self.services.keys().map(|k| k.to_string()).collect()),
+                vec![],
+            ),
         }
     }
 }
