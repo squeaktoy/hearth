5773fa963047da4d8506cf084da874325838a9f8
marceline-cramer <cramermarceline@gmail.com>
2024-01-22 17:48:54 -0500

    kindling-init: comment code flow

--- kindling/init/src/lib.rs
+++ kindling/init/src/lib.rs
@@ -23,10 +23,13 @@ use kindling_host::{prelude::*, registry::Registry};
 use kindling_utils::registry::*;
 use petgraph::{algo::toposort, prelude::DiGraph};
 use serde::Deserialize;
+
 hearth_guest::export_metadata!();
 
+/// The subpath within the filesystem root where services are scanned.
 const SEARCH_DIR: &str = "init";
 
+/// A persistent service container object.
 pub struct Service {
     /// A capability to this process, stays as `None` until this process is started.
     pub process: Option<Capability>,
@@ -56,36 +59,58 @@ impl Service {
 #[no_mangle]
 pub extern "C" fn run() {
     info!("Hello world!");
+
+    // first of all, enumerate available native services
     let native_services = REGISTRY.list_services();
+
+    // add all guest services into a dependency graph structure
     let mut graph = DiGraph::<Service, ()>::new();
+
+    // map of service names to indices within the graph
     let mut names_to_idxs = HashMap::new();
+
+    // list all service directories
     for file in list_files(SEARCH_DIR).unwrap() {
         info!("file: {}", file.name);
+
+        // attempt to parse config
         let Some(config) = get_config(&file.name) else {
             error!("Failed to get config");
             continue;
         };
+
         info!("config: {:?}", config);
 
+        // add service node to graph
         let name = file.name;
         let service = Service::new(name.clone(), config);
         let idx = graph.add_node(service);
         names_to_idxs.insert(name, idx);
     }
 
+    // add dependency edges to graph
     for idx in graph.node_indices() {
         let node = graph.node_weight(idx).unwrap();
         let name = node.name.clone();
         info!("Collecting dependencies of service \'{name}\'");
 
+        // track whether this service has any missing deps
         let mut remove = false;
+
+        // iterate all needed deps
         for dep in node.config.dependencies.need.clone() {
             match names_to_idxs.get(&dep.clone()) {
+                // is this service an existing guest process?
                 Some(dep_idx) => {
                     graph.add_edge(*dep_idx, idx, ());
                 }
+                // guest service not found
                 None => {
+                    // check if the service is native
+                    // if the service is native, we skip adding this edge, and
+                    // its capability will be retrieved during service startup
                     if !native_services.contains(&dep) {
+                        // if it isn't, this dep is missing
                         remove = true;
                         error!("Dependency \'{dep}\' not found");
                     }
@@ -93,33 +118,47 @@ pub extern "C" fn run() {
             };
         }
 
+        // if this service can't start, remove the service from the graph
         if remove {
             info!("Service \'{name}\' will not be spawned");
             graph.remove_node(idx);
+            names_to_idxs.remove(&name);
         }
     }
 
-    // panics on cyclic dependencies
+    // order service graph so that dependencies start before dependents
+    // panic may occur here if the dep graph has a cycle
     let sorted_services = toposort(&graph, None).unwrap();
 
-    // unnecessary but cleans up the code more
+    // create a cache of service names to their started capabilities
     let mut names_to_caps: HashMap<String, Capability> = HashMap::new();
 
-    // add native services to the map
+    // populate the cache with caps to the native services
     for service in native_services {
         let cap = REGISTRY.get_service(&service).unwrap();
         names_to_caps.insert(service, cap);
     }
 
+    // start up all guest services in dependency order
     for idx in sorted_services {
+        // get service data
         let service = graph.node_weight_mut(idx).unwrap();
+
+        // create associated list of all deps' caps
         let mut deps = Vec::new();
         for dep in service.config.dependencies.need.clone() {
+            // look up service cap (either guest or host)
             let cap = names_to_caps.get(&dep).unwrap().to_owned();
             deps.push((dep, cap));
         }
+
+        // create a new registry with this service's deps
         let registry = Some(RegistryServer::spawn(deps));
+
+        // spawn the service
         let cap = service.spawn(registry);
+
+        // provide this service to its dependents
         names_to_caps.insert(service.name.clone(), cap);
     }
 }
