0e719c207e50c44dc172960d0781ac49ed58d221
marceline-cramer <cramermarceline@gmail.com>
2024-01-31 11:11:56 -0700

    kindling-host: add ObjectConfig and Object

--- kindling/host/src/renderer.rs
+++ kindling/host/src/renderer.rs
@@ -18,8 +18,8 @@
 
 use super::*;
 
-use glam::Vec3;
-use hearth_guest::renderer::*;
+use glam::{Mat4, Vec3};
+use hearth_guest::{renderer::*, Lump};
 
 lazy_static::lazy_static! {
     static ref RENDERER: RequestResponse<RendererRequest, RendererResponse> =
@@ -85,5 +85,70 @@ impl DirectionalLight {
     /// Set this distanceal light's distance.
     pub fn set_distance(&self, distance: f32) {
         self.update(DirectionalLightUpdate::Distance(distance));
+    }
+}
+
+/// Configuration for the creation of an [Object].
+#[derive(Clone, Debug)]
+pub struct ObjectConfig<'a> {
+    /// A reference to the lump containing this object's [MeshData].
+    pub mesh: &'a Lump,
+
+    /// An optional list of skeleton joint matrices for this object.
+    pub skeleton: Option<Vec<Mat4>>,
+
+    /// The lump containing this object's [MaterialData].
+    pub material: &'a Lump,
+
+    /// The initial transform of this object.
+    pub transform: Mat4,
+}
+
+/// An object.
+pub struct Object(Capability);
+
+impl Drop for Object {
+    fn drop(&mut self) {
+        self.0.kill();
+    }
+}
+
+impl Object {
+    /// Create a new object in the scene with the given [ObjectConfig].
+    pub fn new(config: ObjectConfig) -> Self {
+        let (result, caps) = RENDERER.request(
+            RendererRequest::AddObject {
+                mesh: config.mesh.get_id(),
+                skeleton: config.skeleton,
+                material: config.material.get_id(),
+                transform: config.transform,
+            },
+            &[],
+        );
+
+        let _ = result.expect("failed to create object");
+
+        Self(caps.first().unwrap().clone())
+    }
+
+    /// Updates the transform of this object.
+    pub fn set_transform(&self, transform: Mat4) {
+        self.0.send(&ObjectUpdate::Transform(transform), &[]);
+    }
+
+    /// Update the joint matrices of this mesh.
+    pub fn set_joint_matrices(&self, joints: Vec<Mat4>) {
+        self.0.send(&ObjectUpdate::JointMatrices(joints), &[]);
+    }
+
+    /// Update the joint transforms of this mesh.
+    pub fn set_joint_transforms(&self, joint_global: Vec<Mat4>, inverse_bind: Vec<Mat4>) {
+        self.0.send(
+            &ObjectUpdate::JointTransforms {
+                joint_global,
+                inverse_bind,
+            },
+            &[],
+        );
     }
 }
